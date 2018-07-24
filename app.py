f69fe31a46cdb982e0cd2484efc4ee263b21640e
Sarah Loh <sarah.loh0708@gmail.com>
2018-07-24 13:34:48 +0100

    Connect flask to mongodb

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 
 import os
-from flask import Flask
+from flask import Flask , render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'sl_task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello World ... again'
+@app.route('/get_tasks')
+def get_tasks():
+    """
+    Display the tasks colection on tasks.html
+    """
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'),
