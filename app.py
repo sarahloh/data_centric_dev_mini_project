642cdcfd27791a8bbe7e5ccdc1f1eb6250b67b36
Sarah Loh <sarah.loh0708@gmail.com>
2018-07-24 18:47:02 +0100

    Create addtask.html and add route to app.py

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ def get_tasks():
     """
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'),
